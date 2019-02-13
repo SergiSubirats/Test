925be592bbd10d1253cc08c3137d5f29131e0bd8
Sergi Subirats <sergi.subirats@enti.cat>
2019-02-13 13:24:11 +0100

    Character
    Art and 1st script of control

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1);
+        }
+
+    }
+}
